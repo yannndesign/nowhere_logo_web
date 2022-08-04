ec211c9f6f0b131ee3f5df15ac2cefa3d41c2865
yannndesign <110218906+yannndesign@users.noreply.github.com>
2022-08-04 01:09:52 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -84,8 +84,8 @@ let tint=80;
   let cc=125;
 let sliderspace=100;
 function setup() {
-//   createCanvas(windowWidth, windowWidth*0.55);
-  createCanvas(windowWidth, windowWidth*0.55).parent('p5');
+  createCanvas(windowWidth, windowWidth*0.55);
+//   createCanvas(windowWidth, windowWidth*0.55).parent('p5');
   //  let button =createButton("reset sketch");
   // button.mousePressed(resetSketch);
 
